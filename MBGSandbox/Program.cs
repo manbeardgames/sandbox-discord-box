aa5bfdf34b7dcb0adf0b1ca9fadfc17942cb8b9e
whitleych17 <whitleych17@ecu.edu>
2019-01-11 11:59:21 -0500

    Add project files.

---
+++ MBGSandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MBGSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
